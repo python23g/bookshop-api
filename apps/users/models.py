f6415117fccaf1395861b7c72354669aa00aa5f8
djumanov <diyorbekjumanov1120@gmail.com>
2023-11-16 10:05:09 +0500

    done

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    
+    def __str__(self):
+        return self.user.username
+    
